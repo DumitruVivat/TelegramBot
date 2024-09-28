95d512f738c28f3fcb438fc85d9dfc4a7db38d01
Dumitru Vivat <kaibaman94@gmail.com>
2024-09-28 17:35:46 +0300

    add telegram update controller

---
+++ dispatcher/src/main/java/ru/relex/controller/UpdateController.java
@@ -0,0 +1,15 @@
+package ru.relex.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j
+public class UpdateController {
+
+    private TelegramBot telegramBot;
+
+    public void registerBot(TelegramBot telegramBot){
+        this.telegramBot = telegramBot;
+    }
+}
