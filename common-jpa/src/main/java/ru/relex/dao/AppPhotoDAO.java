977e4ed302afcda7893ece7819f517dd6e4b1083
Dumitru Vivat <kaibaman94@gmail.com>
2024-10-05 17:13:43 +0300

    add rest module

--- common-jpa/src/main/java/ru/relex/dao/AppPhotoDAO.java
+++ common-jpa/src/main/java/ru/relex/dao/AppPhotoDAO.java
@@ -3,5 +3,5 @@ package ru.relex.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.relex.entity.AppPhoto;
 
-public interface AppPhotoDAO extends JpaRepository<AppPhoto, Integer> {
+public interface AppPhotoDAO extends JpaRepository<AppPhoto, Long> {
 }
