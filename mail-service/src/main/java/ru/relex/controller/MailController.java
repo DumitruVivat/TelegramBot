e523059af87c6bbf17198277892aebb8716af259
Dumitru Vivat <kaibaman94@gmail.com>
2024-10-06 16:39:53 +0300

    add mail module

---
+++ mail-service/src/main/java/ru/relex/controller/MailController.java
@@ -0,0 +1,23 @@
+package ru.relex.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.relex.dto.MailParams;
+import ru.relex.service.MailSenderService;
+
+@RequestMapping("/mail")
+@RestController
+public class MailController {
+    private final MailSenderService mailSenderService;
+
+    public MailController(MailSenderService mailSenderService) {
+        this.mailSenderService = mailSenderService;
+    }
+
+    public ResponseEntity<?> sendActivationMail(@RequestBody MailParams mailParams) {
+        mailSenderService.send(mailParams);
+        return ResponseEntity.ok().build();
+    }
+}
